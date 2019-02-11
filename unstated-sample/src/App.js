e388c4d51bf0d3a7d08d267f2fde8f8d42974f17
luca <luca.dev.math@gmail.com>
2019-02-11 19:13:49 +0900

    initial commit

---
+++ unstated-sample/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container, Subscribe} from "unstated";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        unstated-sample
+      </div>
+    );
+  }
+}
+
+export default App;
